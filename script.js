3c0d1b9c5661304bbb23701a44c7980ebb934497
ed_bolma <ed.bolma21@gmail.com>
2022-12-03 11:05:03 +0100

    the task is complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-playRound(computerPlay(), userPlay());
+playGame(5);
 
 function computerPlay() {
   let randomNumber = Math.random();
@@ -58,6 +58,7 @@ function playRound(computerSelection, playerSelection) {
     (playerSelection == "scissors" && computerSelection == "rock")
   ) {
     console.log("you lose, Paper beats Rock");
+    return { player: 0, computer: 1 };
   }
 
   if (
@@ -66,5 +67,39 @@ function playRound(computerSelection, playerSelection) {
     (playerSelection == "scissors" && computerSelection == "paper")
   ) {
     console.log("you won, Rock beats Scissors");
+    return { player: 1, computer: 0 };
+  }
+}
+
+function playGame(roundQuantity) {
+  let totalScore = { player: 0, computer: 0 };
+
+  let userScore = totalScore.player;
+  let computerScore = totalScore.computer;
+
+  for (let i = 0; i < roundQuantity; i += 1) {
+    console.log("round", i + 1);
+
+    const roundScore = playRound(computerPlay(), userPlay());
+
+    userScore = userScore + roundScore.player;
+    computerScore = computerScore + roundScore.computer;
+
+    console.log(
+      `Round ${
+        i + 1
+      }, Player score: ${userScore} and Computer score ${computerScore}`
+    );
+  }
+
+  if (userScore < computerScore) {
+    console.log(`You lose with score ${userScore}:${computerScore}`);
+  }
+  if (userScore > computerScore) {
+    console.log(`You win with score ${userScore}:${computerScore}`);
+  }
+
+  if (userScore == computerScore) {
+    console.log(`It's a draw with score ${userScore}:${computerScore}`);
   }
 }
